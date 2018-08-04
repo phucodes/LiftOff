7ab91fa17ecc7f0fe530c4d07b4bc871ab5e6c8a
phudinhn <phudinhn@gmail.com>
2018-08-04 17:03:58 -0500

    Add Seed for user roles.

---
+++ LiftOff/Data/Seed.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using LiftOff.Models;
+
+namespace LiftOff.Data
+{
+    public static class Seed
+    {
+        public static async Task CreateRoles(IServiceProvider serviceProvider, IConfiguration Configurtion)
+        {
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            string[] roleNames = { "Employer", "User" };
+            IdentityResult roleResult;
+
+            foreach (var roleName in roleNames)
+            {
+                var roleExists = await RoleManager.RoleExistsAsync(roleName);
+
+                if (!roleExists)
+                {
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+        }
+    }
+}
