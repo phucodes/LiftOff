b7b40d18035d6d523aad1e893936d103ac3739a5
phudinhn <phudinhn@gmail.com>
2018-08-04 20:33:23 -0500

    Add migrations.

---
+++ LiftOff/Controllers/RoleController.cs
@@ -0,0 +1,17 @@
+﻿/* using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiftOff.Controllers
+{
+    public class RoleController: Controller
+    {
+        public IActionResult Index()
+        {
+
+        }
+    }
+}
+*/
--- LiftOff/Data/Migrations/20180805002553_Init.Designer.cs
+++ LiftOff/Data/Migrations/20180805002553_Init.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace LiftOff.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180805002553_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
