3b7a5676ef43cf851a695e7087f6b822dab78347
phudinhn <phudinhn@gmail.com>
2018-08-07 18:40:57 -0500

    More Job database update

--- LiftOff/Migrations/20180807214020_OpenClose.Designer.cs
+++ LiftOff/Migrations/20180807214020_OpenClose.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace LiftOff.Migrations
 {
     [DbContext(typeof(JobDbContext))]
-    partial class JobDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180807214020_OpenClose")]
+    partial class OpenClose
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LiftOff/Migrations/20180807214020_OpenClose.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace LiftOff.Migrations
+{
+    public partial class OpenClose : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOpened",
+                table: "Job",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsOpened",
+                table: "Job");
+        }
+    }
+}
