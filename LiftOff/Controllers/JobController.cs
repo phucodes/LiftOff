777bdd09452ddbc7efb32d6989808fbcaa2caada
phudinhn <phudinhn@gmail.com>
2018-08-07 04:22:10 -0500

    Update JobController

--- LiftOff/Controllers/JobController.cs
+++ LiftOff/Controllers/JobController.cs
@@ -8,12 +8,16 @@ using Microsoft.EntityFrameworkCore;
 using LiftOff.Models.JobViewModels;
 using LiftOff.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace LiftOff.Controllers
 {
     [Authorize(Roles = "Employer")]
     public class JobController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
         private JobDbContext context;
 
         public JobController(JobDbContext dbContext)
@@ -34,6 +38,8 @@ namespace LiftOff.Controllers
         {
             if (ModelState.IsValid)
             {
+                // var currentUser = _userManager.GetUserName(HttpContext.User);
+                var currentUser = HttpContext.User.Identity.Name;
                 Job newJob = new Job()
                 {
                     Name = addJobViewModel.Name,
@@ -41,16 +47,34 @@ namespace LiftOff.Controllers
                     Location = addJobViewModel.Location,
                     PositionType = addJobViewModel.PositionType,
                     PositionLevel = addJobViewModel.PositionLevel,
-                    Description = addJobViewModel.Description
+                    Description = addJobViewModel.Description,
+                    Employer = currentUser
                 };
                 context.Job.Add(newJob);
                 context.SaveChanges();
 
-                return Redirect(String.Format("/job?id={0}", newJob.Id));
+                return RedirectToAction("ViewJob", new { id = newJob.JobId });
             }
 
             // If we get here, something's wrong and re-render the form
             return View(addJobViewModel);
+        }
+
+        [HttpGet]
+        public IActionResult ViewJob(int id)
+        {
+            Job viewJob = context.Job.Find(id);
+            AddJobViewModel currentJob = new AddJobViewModel
+            {
+                Name = viewJob.Name,
+                DatePosted = viewJob.DatePosted,
+                Location = viewJob.Location,
+                PositionType = viewJob.PositionType,
+                PositionLevel = viewJob.PositionLevel,
+                Description = viewJob.Description
+            };
+
+            return View(viewJob);
         }
     }
 }
