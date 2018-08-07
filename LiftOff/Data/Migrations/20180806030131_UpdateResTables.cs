e8a9d7a2318b15b1fc20ee62ab26856b71a25319
phudinhn <phudinhn@gmail.com>
2018-08-06 22:53:52 -0500

    Add migrations for new job feature, seperate DbContext

---
+++ LiftOff/Data/Migrations/20180806030131_UpdateResTables.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace LiftOff.Data.Migrations
+{
+    public partial class UpdateResTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Job",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DatePosted = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Job", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Job");
+        }
+    }
+}
---
+++ LiftOff/Data/Migrations/20180807005716_UpdateJob.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace LiftOff.Data.Migrations
+{
+    public partial class UpdateJob : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Job",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PositionLevel",
+                table: "Job",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostitionType",
+                table: "Job",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Job");
+
+            migrationBuilder.DropColumn(
+                name: "PositionLevel",
+                table: "Job");
+
+            migrationBuilder.DropColumn(
+                name: "PostitionType",
+                table: "Job");
+        }
+    }
+}
