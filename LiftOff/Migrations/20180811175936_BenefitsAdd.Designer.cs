bb40e3773c901a7cecd7f19768c1ec163a2f278a
phudinhn <phudinhn@gmail.com>
2018-08-11 13:24:28 -0500

    Additonal Migrations

--- LiftOff/Migrations/20180811175936_BenefitsAdd.Designer.cs
+++ LiftOff/Migrations/20180811175936_BenefitsAdd.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace LiftOff.Migrations
 {
     [DbContext(typeof(JobDbContext))]
-    partial class JobDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180811175936_BenefitsAdd")]
+    partial class BenefitsAdd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LiftOff/Migrations/20180811175936_BenefitsAdd.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace LiftOff.Migrations
+{
+    public partial class BenefitsAdd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Benefits",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BenefitName = table.Column<string>(nullable: true),
+                    JobId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Benefits", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Benefits_Job_JobId",
+                        column: x => x.JobId,
+                        principalTable: "Job",
+                        principalColumn: "JobId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Benefits_JobId",
+                table: "Benefits",
+                column: "JobId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Benefits");
+        }
+    }
+}
