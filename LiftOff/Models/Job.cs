b24ff0369d7a8e39a2ed0c81b2f85d0f8f1c8f83
phudinhn <phudinhn@gmail.com>
2018-08-11 14:02:47 -0500

    Update Job Model

--- LiftOff/Models/Job.cs
+++ LiftOff/Models/Job.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -28,6 +29,8 @@ namespace LiftOff.Models
         public List<Requirement> Requirements { get; set; }
 
         public List<Benefit> Benefits { get; set; }
+
+        public List<ApplicationUser> Applicants { get; set; }
     }
 
     public class Requirement
