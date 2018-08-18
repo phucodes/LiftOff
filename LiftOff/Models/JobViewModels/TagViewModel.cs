bbfd8d0541954bb03ed0b55bb46485c5d180f180
phudinhn <phudinhn@gmail.com>
2018-08-18 13:08:23 -0500

    Add TagViewModel

---
+++ LiftOff/Models/JobViewModels/TagViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiftOff.Models.JobViewModels
+{
+    public class TagViewModel
+    {
+        [Required]
+        [Display(Name = "Requirement Names")]
+        public string TagName { get; set; }
+
+        [Microsoft.AspNetCore.Mvc.HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+
+        public int JobId { get; set; }
+    }
+}
